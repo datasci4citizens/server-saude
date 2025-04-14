1277fb90fedd961306f6bf53fc4d60389830d439
André Satorres <aa.satorres@gmail.com>
2025-04-14 12:53:04 -0300

    feat(SAU-22): Google oauth

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'perf']
-      ],
-      'scope-empty': [2, 'never'],
-      'scope-pattern': [2, 'always', '^SAU-[0-9]+$'],
-      'subject-empty': [2, 'never'],
-      'subject-case': [0],
-      'header-pattern': [2, 'always', /^(\w+)\((SAU-\d+)\): (.+)$/],
-    },
-  };
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'perf']
+    ],
+    'scope-empty': [2, 'never'],
+    'scope-case': [0],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0],
+  },
+};
