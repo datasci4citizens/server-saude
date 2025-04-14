40428a4bc3b1306aea80c1bb66724a2e9c316f8b
André Satorres <aa.satorres@gmail.com>
2025-04-14 12:27:22 -0300

    test

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'perf']
+      ],
+      'scope-empty': [2, 'never'],
+      'scope-pattern': [2, 'always', '^SAU-[0-9]+$'],
+      'subject-empty': [2, 'never'],
+      'subject-case': [0],
+      'header-pattern': [2, 'always', /^(\w+)\((SAU-\d+)\): (.+)$/],
+    },
+  };
+  
