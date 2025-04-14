a16e2d4b01d8848a04a79ee6fdaf431edf059651
André Satorres <aa.satorres@gmail.com>
2025-04-14 12:17:40 -0300

    feat(SAU-22): Google OAuth + CORS
    - Add Google OAuth login and test endpoint
    
    - Add CORS for frontend coms

--- core/views.py
+++ core/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+
+# Just a test endpoint to check if the user is logged in and return user info
+@login_required
+def me(request):
+    user = request.user
+    return JsonResponse({
+        'username': user.username,
+        'email': user.email,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+    })
