1cc9189da906d5fb3edb0b6f2f4efacb798c417c
Jun <gustavogujun@gmail.com>
2025-04-21 12:52:34 -0300

    Add tables to admin site

--- app_saude/admin.py
+++ app_saude/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Provider, Person, Concept, CareSite, Observation, Emergencymessage, DrugExposure, Address, LinkedProvider, EmergencyProvider, ProviderCareSite, ProviderConcept, VisitOccurrence
+
+admin.site.register(Provider)
+admin.site.register(Person)
+admin.site.register(Concept)
+admin.site.register(CareSite)
+admin.site.register(Observation)
+admin.site.register(Emergencymessage)
+admin.site.register(DrugExposure)
+admin.site.register(Address)
+admin.site.register(LinkedProvider)
+admin.site.register(EmergencyProvider)
+admin.site.register(ProviderCareSite)
+admin.site.register(ProviderConcept)
+admin.site.register(VisitOccurrence)
